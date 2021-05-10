696afba6c43d0d4eb21fc86b964d2d6a1a98e55e
Alexander Soare <alexander.soare159@gmail.com>
2021-05-10 12:31:39 +0100

    deal with single channel images as well

--- pytorch_custom/image_utils.py
+++ pytorch_custom/image_utils.py
@@ -10,8 +10,13 @@ def normalize(img: np.ndarray, div255=True) -> np.ndarray:
     img = img.astype(float)
     if div255:
         img /= 255
-    img -= np.array([0.485, 0.456, 0.406])
-    img /= np.array([0.229, 0.224, 0.225])
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    if len(img.shape)  == 2 or img.shape[2] == 1:
+        mean = mean.mean()
+        std = std.mean()
+    img -= mean
+    img /= std
     return img
 
 
@@ -21,7 +26,12 @@ def denormalize(img: np.ndarray) -> np.ndarray:
     expects image with float pixels as input
     returns np.uint8 type
     """
-    img *= np.array([0.229, 0.224, 0.225])
-    img += np.array([0.485, 0.456, 0.406])
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    if len(img.shape) == 2 or img.shape[2] == 1:
+        mean = mean.mean()
+        std = std.mean()
+    img *= std
+    img += mean
     img *= 255
     return img.astype(np.uint8)
