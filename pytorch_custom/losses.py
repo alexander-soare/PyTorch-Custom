6e47256499c019312148abeffc2ab34002206b8e
Alexander Soare <alexander.soare159@gmail.com>
2021-05-28 20:38:18 +0100

    fix focal loss

--- pytorch_custom/losses.py
+++ pytorch_custom/losses.py
@@ -1,4 +1,5 @@
 import torch
+from torch import LongTensor, FloatTensor
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
@@ -7,15 +8,14 @@ from torch.nn.modules.loss import _Loss
 # 1. Consistency
 # 2. See if we can make use of pytorch-metric-learning instead of these
 
-# https://github.com/mathiaszinnen/focal_loss_torch/blob/main/focal_loss/focal_loss.py
+
 class FocalLoss(nn.Module):
-    """
-    I've modified this in the following ways:
-    1. with_logits logic. WARNING - not yet tested for multi-class
-    2. with_binary logic
-    """
-    def __init__(self, alpha=1, gamma=2, with_logits=False, binary=False,
-                    reduction: str = 'mean'):
+    def __init__(self, alpha=0.25, gamma=2, with_logits=True, reduction: str = 'mean'):
+        """
+        https://github.com/mathiaszinnen/focal_loss_torch/blob/main/focal_loss/focal_loss.py
+        https://arxiv.org/pdf/1708.02002.pdf
+        default gamma from tabel 1(b)
+        """
         super().__init__()
         if reduction not in ['mean', 'none', 'sum']:
             raise NotImplementedError('Reduction {} not implemented.'.format(reduction))
@@ -23,19 +23,53 @@ class FocalLoss(nn.Module):
         self.alpha = alpha
         self.gamma = gamma
         self.with_logits = with_logits
-        self.binary = binary
 
-    def forward(self, y_pred, y_true):
+    def forward(self, input, target):
+        """
+        input to be (B, 1) of probabilites (not logits)
+        """
         if self.with_logits:
-            if self.binary:
-                y_pred = torch.sigmoid(y_pred)
-            else:
-                # WARNING not tested yet
-                y_pred = torch.softmax(y_pred, -1)
-        # WARNING this assumes we are not doing label smoothing
-        p_t = torch.where(y_true == 1, y_pred, 1-y_pred)
+            input = F.sigmoid(input)
+        p_t = torch.where(target == 1, input, 1-input)
         fl = - 1 * (1 - p_t) ** self.gamma * torch.log(p_t)
-        fl = torch.where(y_true == 1, fl * self.alpha, fl)
+        # https://github.com/mathiaszinnen/focal_loss_torch/issues/2
+        fl = torch.where(target == 1, fl * self.alpha, fl * (1 - self.alpha))
+        return self._reduce(fl)
+
+    def _reduce(self, x):
+        if self.reduction == 'mean':
+            return x.mean()
+        elif self.reduction == 'sum':
+            return x.sum()
+        else:
+            return x
+
+
+class CategoricalFocalLoss(nn.Module):
+    def __init__(self, gamma:float=2., reduction:str='mean'):
+        """
+        https://arxiv.org/pdf/1708.02002.pdf
+        default gamma from table 1(b)
+        we could specify alpha as a per class weighting but I haven't implemented
+         it yet
+        """
+        super().__init__()
+        if reduction not in ['mean', 'none', 'sum']:
+            raise NotImplementedError('Reduction {} not implemented.'.format(reduction))
+        self.reduction = reduction
+        self.gamma = gamma
+
+    def forward(self, input: FloatTensor, target: LongTensor):
+        """
+        expect input to be (B, N) where N is number of classes
+        expect target to be (B, 1) If target is B, unsqueeze
+        """
+        target = target.reshape(target.shape[0], -1)
+        p = torch.softmax(input, -1)
+        p = torch.gather(p, -1, target)
+        logp = torch.log_softmax(input, -1)
+        logp = torch.gather(logp, -1, target)
+        fl = - 1 * (1 - p) ** self.gamma * logp
         return self._reduce(fl)
 
     def _reduce(self, x):
@@ -63,16 +97,15 @@ class CELabelSmoothingLoss(nn.Module):
         self.n_classes = n_classes
         self.dim = dim
 
-    def forward(self, pred, target):
-        """ expects target to be categorical, and pred to be logits
+    def forward(self, input, target):
+        """ expects target to be categorical, and input to be logits
         """
-        pred = pred.log_softmax(dim=self.dim)
+        x = input.log_softmax(dim=self.dim)
         with torch.no_grad():
-            true_dist = torch.zeros_like(pred)
+            true_dist = torch.zeros_like(x)
             true_dist.fill_(self.smoothing / (self.n_classes - 1))
             true_dist.scatter_(1, target.data.unsqueeze(1), 1.0 - self.smoothing)
-        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
-
+        return torch.mean(torch.sum(-true_dist * x, dim=self.dim))
 
 
 class BCELabelSmoothingLoss(nn.Module):
@@ -84,15 +117,15 @@ class BCELabelSmoothingLoss(nn.Module):
         `smoothing` is the smoothing factor. How much less confident than 100%
          are we on true labels?
         """
-        super(BCELabelSmoothingLoss, self).__init__()
+        super().__init__()
         self.smoothing = smoothing
 
-    def forward(self, pred, target):
-        """ expects target to be binary, and pred to be logits
+    def forward(self, input, target):
+        """ expects target to be binary, and input to be logits
         """
         with torch.no_grad():
             target = torch.abs(target - self.smoothing)
-        return F.binary_cross_entropy_with_logits(pred, target)
+        return F.binary_cross_entropy_with_logits(input, target)
 
 
 # https://discuss.pytorch.org/t/label-smoothing-with-ctcloss/103392
