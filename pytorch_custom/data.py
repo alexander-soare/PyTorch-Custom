2457ad17de6abb7089bd225adb8f90c2abe16fc6
Alexander Soare <alexander.soare159@gmail.com>
2021-09-07 08:58:28 +0100

    handle df with multiple rows per file path

--- pytorch_custom/data.py
+++ pytorch_custom/data.py
@@ -13,7 +13,7 @@ from .image_utils import normalize
 
 class ImageDataset(torch.utils.data.Dataset):
     """
-    most basic dataset for image task
+    Most basic dataset for image task
     """
     def __init__(self, df, transforms=[]):
         self.file_paths = list(df.file_path)
@@ -44,67 +44,74 @@ class ImageDataset(torch.utils.data.Dataset):
 class DataLoaders:
     """
     Convenience class for making data loaders for training and optionally also
-     validation. Built in k-fold, stratified k-fold, and weighted sampling
-     functionality
-    Expects df with at least a file_path col
-    If stratified_split=True, then expects a stratify group column
+    validation. Built in k-fold, stratified k-fold, and weighted sampling
+    functionality
+    Expects df with at least a file_path col. We consider **unique** file paths
+    meaning splits are done according to file path not df row.
+    If stratified_split=True, then expects stratify_group column.
     If do_balance=True, expects a sampling_weight column where this weight refers
-     to how that type of row should be sampled
+    to how that type of row should be sampled
     """
-    def __init__(self, dataset_class, df, train_batch_size, train_dataset_args=[],
-                    train_dataset_kwargs={}, val_dataset_args=[], 
-                    val_dataset_kwargs = {}, val_batch_size=None, num_folds=0,
-                    fold_ix=0, split_seed=None, do_stratify=True,
-                    train_transforms=[], val_transforms=[], do_balance=False,
-                    num_workers=0):
+    def __init__(
+            self, dataset_class, df, train_batch_size, train_dataset_args=[],
+            train_dataset_kwargs={}, val_dataset_args=[],
+            val_dataset_kwargs = {}, val_batch_size=None, num_folds=0,
+            fold_ix=0, split_seed=None, do_stratify=True, train_transforms=[],
+            val_transforms=[], do_balance=False, num_workers=0):
         
         if val_batch_size is None and num_folds > 0:
             val_batch_size = train_batch_size
 
+        file_paths = np.array(df.file_path.unique())
         if num_folds > 0:
             kfold_class = StratifiedKFold if do_stratify else KFold
             kf = kfold_class(n_splits=num_folds, shuffle=True,
-                                random_state=split_seed)
+                             random_state=split_seed)
             if do_stratify:
-                train_ix, val_ix = next(islice(kf.split(df.file_path,
-                            df.stratify_group), fold_ix, fold_ix+1))
-            else:
+                assert len(df.drop_duplicates(
+                    subset=['file_path', 'stratify_group'])) \
+                    == len(file_paths), ("It is required that file_path and "
+                                         "stratify_group are uniquely paired "
+                                         "for a stratified split.")
                 train_ix, val_ix = next(islice(kf.split(
-                                        df.file_path), fold_ix, fold_ix+1))
+                    df.file_path, df.stratify_group), fold_ix, fold_ix+1))
+            else:
+                
+                train_ix, val_ix = next(islice(kf.split(
+                    file_paths), fold_ix, fold_ix+1))
+            train_file_paths = set(file_paths[train_ix])
+            val_file_paths = set(file_paths[val_ix])
         else:
-            train_ix = np.arange(len(df))
+            train_file_paths = file_paths
             
-        self.train_dataset = dataset_class(df.iloc[train_ix],
-                                            *train_dataset_args,
-                                            transforms=train_transforms,
-                                            **train_dataset_kwargs)
+        self.train_dataset = dataset_class(
+            df[df.file_path.isin(train_file_paths)], *train_dataset_args,
+            transforms=train_transforms, **train_dataset_kwargs)
         if do_balance:
             train_sampler = WeightedRandomSampler(
-                        torch.Tensor(list(df.iloc[train_ix].sampling_weight)),
-                        len(self.train_dataset))
+                torch.Tensor(list(df[
+                    df.file_path.isin(train_file_paths)].sampling_weight)),
+                len(self.train_dataset))
         else:
             train_sampler = None
 
         self.train_loader = DataLoader(self.train_dataset,
-                        batch_size=train_batch_size, shuffle=(not do_balance),
-                        num_workers=num_workers, pin_memory=True,
-                        sampler=train_sampler,
-                        collate_fn=getattr(self.train_dataset, 'collate_fn', None))
-
+            batch_size=train_batch_size, shuffle=(not do_balance),
+            num_workers=num_workers, pin_memory=True, sampler=train_sampler,
+            collate_fn=getattr(self.train_dataset, 'collate_fn', None))
 
         if num_folds > 0:
-            self.val_dataset = dataset_class(df.iloc[val_ix],
-                                            *val_dataset_args,
-                                            transforms=val_transforms,
-                                            **val_dataset_kwargs)
+            self.val_dataset = dataset_class(
+                df[df.file_path.isin(val_file_paths)], *val_dataset_args,
+                transforms=val_transforms, **val_dataset_kwargs)
             if do_balance:
                 val_sampler = WeightedRandomSampler(
-                        torch.Tensor(list(df.iloc[val_ix].sampling_weight)),
-                        len(self.val_dataset))
+                    torch.Tensor(list(df[
+                        df.file_path.isin(val_file_paths)].sampling_weight)),
+                    len(self.val_dataset))
             else:
                 val_sampler = None
-            self.val_loader = DataLoader(self.val_dataset,
-                                    batch_size=val_batch_size, shuffle=False,
-                                    num_workers=num_workers, pin_memory=True,
-                                    sampler=val_sampler, 
-                                    collate_fn=getattr(self.val_dataset, 'collate_fn', None))
+            self.val_loader = DataLoader(
+                self.val_dataset, batch_size=val_batch_size, shuffle=False,
+                num_workers=num_workers, pin_memory=True, sampler=val_sampler, 
+                collate_fn=getattr(self.val_dataset, 'collate_fn', None))
